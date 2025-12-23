451bed59131239885dc1a96e83bfd402cb8a9c2f
PHPMake <phpmake.github@gmail.com>
2025-12-23 12:37:28 +0330

    Update rector.php

--- rector.php
+++ rector.php
@@ -7,23 +7,24 @@ use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/bin',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__ . '/.',
     ])
+    // Skip non-project directories.
     ->withSkip([
         __DIR__ . '/.git',
         __DIR__ . '/.github',
+        __DIR__ . '/vendor',
         __DIR__ . '/resources',
         __DIR__ . '/schema',
-        __DIR__ . '/vendor',
     ])
     ->withRootFiles()
     ->withIndent(' ', 4)
-    ->withPhpVersion(PhpVersion::PHP_83)
     ->withComposerBased(phpunit: true)
-    ->withPhpSets(php83: true)
     ->withTypeCoverageLevel(10)
     ->withDeadCodeLevel(10)
     ->withCodeQualityLevel(10)
-    ->withCodingStyleLevel(10);
+    ->withCodingStyleLevel(10)
+    // Force Rector to use PHP x.x features only.
+    ->withPhpVersion(PhpVersion::PHP_83)
+    // Import Rector sets relevant for PHP x.x.
+    ->withPhpSets(php83: true);
